29ccbd7f3a73250b994d74b28e92464d56ed0e89
DovganO <dovgan.olexandr@gmail.com>
2020-12-12 20:04:19 +0200

    ЧВ2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
+/* import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ */
+
 import User from './componets/Profile';
-import Profile from './componets/Profile'
+import Profile from './componets/Profile';
 import users from './user.json';
 
 const user = users;
 
 export default function App() {
-    return (
+  return (
     <div>
-        <User
-    avatar={user.avatar}
-    name={user.name}
-    tag={user.tag}
-    location={user.location}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-  />
+      <User
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
-    );
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
-/* import 'modern-normalize/modern-normalize.css'; */
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import App from './App';
 
 /* const user = users; */
 
 /* function User(props) {
-
   const { avatar, name, tag, location, followers, views, likes } = props;
-
   return (
     <div class="profile">
     <div class="description">
@@ -23,7 +39,6 @@ import App from './App';
       <p class="tag">@{tag}</p>
       <p class="location">{location}</p>
     </div>
-
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
@@ -42,8 +57,8 @@ import App from './App';
   );
 } */
 
-
-{/* <User
+{
+  /* <User
     avatar={user.avatar}
     name={user.name}
     tag={user.tag}
@@ -51,22 +66,7 @@ import App from './App';
     followers={user.stats.followers}
     views={user.stats.views}
     likes={user.stats.likes}
-  />, */}
+  />, */
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-
-/* 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-
